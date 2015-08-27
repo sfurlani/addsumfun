39f231a13379428f660bd302b94060e8cecb55fc
Stephen Furlani <stephen.furlani@gmail.com>
2015-08-27 01:43:45 -0400

    starting to celan up code

--- Add Sum Fun/Add Sum Fun/HomeViewController.swift
+++ Add Sum Fun/Add Sum Fun/HomeViewController.swift
@@ -22,10 +22,7 @@ class HomeViewController: UIViewController {
 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
         
         switch segue.destinationViewController {
         case let vc as PlayViewController:
